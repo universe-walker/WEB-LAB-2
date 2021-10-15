bf3b0b3d808641a474197c27cf66c59e91a09ccd
Nikita <nikita.motlitch@yandex.ru>
2021-10-15 10:05:50 +0300

    Удалил старый код проверки формы комментария

--- js/script.js
+++ js/script.js
@@ -25,54 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
       toggleBlurAllExceptModalWindow();
    });
 
-   /* работа с формой комментария */
-
-   new window.JustValidate('.comment__comment-form', {
-      rules: {
-         comment: {
-            required: true,
-            function: (name, value) => {
-               return value.length > 0;
-            }
-         }
-      },
-      messages: {
-         comment: {
-            required: 'Это поле обязательно'
-         }
-      },
-      submitHandler: function (form, values) {
-         let textarea = form.elements.textarea;
-         let commentText = textarea.value;
-         let commentAuthor = 'Nikita';
-         textarea.value = '';
-
-         let comment = document.createElement('div');
-         comment.className = 'comment__item';
-         let datetime = new Date();
-         let dt = Intl.DateTimeFormat('ru', {
-            day: 'numeric',
-            month: 'numeric',
-            year: 'numeric',
-            hour: "numeric",
-            minute: "numeric"
-         }).format(datetime);
-         comment.innerHTML = `
-            <div class="comment__header">
-               <div class="comment_avatar-wrapper">
-                  <img src="img/avatar.jpg" alt="" class="comment__avatar">
-               </div>
-               <div class="comment__author">${commentAuthor}</div>
-            </div>
-            <div class="comment__text">${commentText}</div>
-            <div class="comment__datetime">${dt}</div>
-         </div>
-         `
-         let mainComment = document.querySelector('.main__comment');
-         mainComment.append(comment);
-      }
-   });
-
    /* валидация регистрации/входа */
 
    new window.JustValidate('.modal__sign-up', {
